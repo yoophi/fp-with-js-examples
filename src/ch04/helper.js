1947b457e935648f030c8b9339f4914b08b4a5a4
Pyunghyuk Yoo <yoophi@gmail.com>
2021-07-16 18:08:09 +0900

    ✨ NEW: ex 4.3 extending the core language

--- src/ch04/helper.js
+++ src/ch04/helper.js
@@ -10,6 +10,28 @@ const checkType = R.curry(function (typeDef, obj) {
   return obj;
 });
 
+const Tuple = function (/* types */) {
+  const typeInfo = Array.prototype.slice.call(arguments, 0);
+  const _T = function (/* values */) {
+    const values = Array.prototype.slice.call(arguments, 0);
+    if (values.some((val) => val === null || val === undefined)) {
+      throw new ReferenceError("Toples may not have any null values");
+    }
+    if (values.length !== typeInfo.length) {
+      throw new TypeError("Tuple arity does not match its prottype");
+    }
+    values.map((val, index) => {
+      this["_" + (index + 1)] = checkType(typeInfo[index], val);
+    }, this);
+    Object.freeze(this);
+  };
+  _T.prototype.values = function () {
+    return Object.keys(this).map((k) => this[k], this);
+  };
+  return _T;
+};
+
 module.exports = {
   checkType,
+  Tuple,
 };
--- src/ch04/tests.js
+++ src/ch04/tests.js
@@ -43,3 +43,37 @@ QUnit.test("Check Type tests", function () {
     checkType(String)(42);
   }, TypeError);
 });
+
+QUnit.test("Tuple test", function () {
+  const { Tuple } = require("./helper");
+  const StringPair = Tuple(String, String);
+  const name = new StringPair("Barkley", "Rosser");
+  let [first, last] = name.values();
+  assert.equal(first, "Barkley");
+  assert.equal(last, "Rosser");
+});
+
+QUnit.test("Extending the core language", function () {
+  String.prototype.first = _.partial(String.prototype.substring, 0, _);
+  let result = "Functional Programming".first(3);
+  assert.equal(result, "Fun");
+
+  String.prototype.asName = _.partial(
+    String.prototype.replace,
+    /(\w+)\s(\w+)/,
+    "$2, $1"
+  );
+  result = "Alonzo Church".asName();
+  assert.equal(result, "Church, Alonzo");
+
+  String.prototype.explode = _.partial(String.prototype.match, /[\w]/gi);
+  result = "ABC".explode();
+  assert.deepEqual(result, ["A", "B", "C"]);
+
+  String.prototype.parseUrl = _.partial(
+    String.prototype.match,
+    /(http[s]?|ftp):\/\/([^:\/\s]+)\.([^:\/\s]{2,5})/
+  );
+  result = "http://example.com".parseUrl();
+  assert.deepEqual(result, ["http://example.com", "http", "example", "com"]);
+});
