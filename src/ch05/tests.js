6becb43d11236664267214fa5e6a683d52fcc62b
Pyunghyuk Yoo <yoophi@gmail.com>
2021-07-20 22:45:40 +0900

    ✨ NEW: ex 5.3 simple find with wrapper

--- src/ch05/tests.js
+++ src/ch05/tests.js
@@ -21,3 +21,13 @@ QUnit.test("Simple functor test", function () {
   assert.equal(five.map(R.identity), 5);
   assert.equal(two.fmap(plus3).fmap(plus10).map(R.identity), 15);
 });
+
+QUnit.test("Simple find with wrapper", function () {
+  const { db } = require("../ch01/helper");
+  const find = R.curry((db, id) => db.find(id));
+  const findStudent = R.curry((db, ssn) => wrap(find(db, ssn)));
+  const getAddress = (student) => wrap(student.fmap(R.prop("firstname")));
+  const studentAddress = R.compose(getAddress, findStudent(db));
+
+  assert.deepEqual(studentAddress("444-44-4444"), wrap(wrap("Alonzo")));
+});
