bef9c04326da4d829a975ecdc0616ea29cfa0a6e
Pyunghyuk Yoo <yoophi@gmail.com>
2021-07-20 22:45:17 +0900

    ✨ NEW: ex 5.2 simple functor test

--- src/ch05/tests.js
+++ src/ch05/tests.js
@@ -11,3 +11,13 @@ QUnit.test("Simple Wrapper test", function () {
   const wrappedValue = wrap("Get Functional");
   assert.equal(wrappedValue.map(R.identity), "Get Functional");
 });
+
+QUnit.test("Simple functor test", function () {
+  const plus = R.curry((a, b) => a + b);
+  const plus3 = plus(3);
+  const plus10 = plus(10);
+  const two = wrap(2);
+  const five = two.fmap(plus3);
+  assert.equal(five.map(R.identity), 5);
+  assert.equal(two.fmap(plus3).fmap(plus10).map(R.identity), 15);
+});
