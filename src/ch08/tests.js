7daf9353e340725fc3c24697c65721e1c288eceb
Pyunghyuk Yoo <yoophi@gmail.com>
2021-07-30 23:32:08 +0900

    ✨ NEW: ex 8.2 generator 2

--- src/ch08/tests.js
+++ src/ch08/tests.js
@@ -14,3 +14,17 @@ QUnit.test("Generator 1", function (assert) {
   assert.equal(adder.next().value, 0);
   assert.equal(adder.next(5).value, 5);
 });
+
+QUnit.test("Generator 2", function (assert) {
+  function* range(start, finish) {
+    for (let i = start; i < finish; i++) {
+      yield i;
+    }
+  }
+
+  let r = range(0, Number.POSITIVE_INFINITY);
+
+  assert.equal(r.next().value, 0);
+  assert.equal(r.next().value, 1);
+  assert.equal(r.next().value, 2);
+});
